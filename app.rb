21159a286365c802233aab41fc6d66868d7b322e
Travis <travis@whitedell.(none)>
2014-11-21 19:38:42 -0500

    showing images of ships and turrets on fleet and unit build pages

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'csv'
 set :bind, '0.0.0.0'
 set :port, 8080
 set :static, true
-set :public_folder, "static"
+set :public_folder, File.dirname(__FILE__) + "/static"
 set :views, "views"
 
 enable :sessions
