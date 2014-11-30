022971c8171d650ba4263b277759c623a58eb494
Travis <travis@whitedell.(none)>
2014-11-30 12:13:03 -0500

    displaying some ships on the screen, wooohoooo

--- static/js/game.js
+++ static/js/game.js
@@ -1,10 +1,20 @@
 // Initialize Phaser and create a 640x640 game
-var game = new Phaser.Game(640, 640, Phaser.CANVAS, 'game_div',
+var this_game = new Phaser.Game(640, 640, Phaser.CANVAS, 'game_div',
    { preload: preload, create: create, update: update });
 
 function preload() {
    var href = document.URL;
    var gameid = href.split('/').pop();
+   // Change the background color of the game
+   this_game.stage.backgroundColor = '#000000';
+   
+   // Load the images that we need
+   this_game.load.image('Cruiser', '/assets/images/cruiser.png');
+   this_game.load.image('Frigate', '/assets/images/frigate.png');
+   this_game.load.image('Battleship', '/assets/images/battleship.png');
+
+   this_game.load.image('move_tile', '/assets/images/move_tile.png');
+   this_game.load.image('attack_tile', '/assets/images/attack_tile.png');
    $.ajax({
       url: "/gamestate",
       data: {
@@ -12,23 +22,11 @@ function preload() {
       },
       dataType: "json",
       type : 'GET',
-      success: function(response) {
-         // create game data object
-      },
+      success: function(data){setupBoard(data);},
       error: function() {
          // give error message
       }
    });
-   // Change the background color of the game
-   game.stage.backgroundColor = '#000000';
-   
-   // Load the images that we need
-   game.load.image('cruiser', '/assets/images/cruiser.png');
-   game.load.image('frigate', '/assets/images/frigate.png');
-   game.load.image('battleship', '/assets/images/battleship.png');
-
-   game.load.image('move_tile', '/assets/images/move_tile.png');
-   game.load.image('attack_tile', '/assets/images/attack_tile.png');
 }
 
 var BOARD_COLS = 10;
@@ -37,7 +35,6 @@ var SHIP_SIZE  = 64;
 
 function create() {
    // Function called after 'preload' to set up the game
-   spawnShips();
 
 }
 
@@ -45,6 +42,18 @@ function update() {
    // get updates on game from server and modify the map on move
 }
 
-function spawnShips() {
+function setupBoard(game_info) {
    // get fleets for both players and render on the board
+   ships = this_game.add.group();
+   fleet = game_info["fleet"]
+   game  = game_info["game"]
+   turrets = game_info["turrets"]
+   for(var i=0; i < fleet.length; i++)  {
+      var obj = fleet[i];
+      this_ship = ships.create(i * 64 + 32, i * 64 + 32, obj["model"]);
+      this_ship.anchor.set(0.5);
+      this_ship.armor = obj["armor"];
+      this_ship.shields = obj["shields"];
+      this_ship.direction = obj["direction"];
+   }
 }
